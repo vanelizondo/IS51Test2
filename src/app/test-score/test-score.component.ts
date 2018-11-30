3bea1b7e2f21eeb53960fea3f3ff7917ac2cad61
endtry <lucas123phan@gmail.com>
2018-11-29 16:12:10 -0800

    remove iterface

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,14 +4,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
-export interface ITest {
-  id?: number;
-  testName: string;
-  pointsPossible: number;
-  pointsReceived: number;
-  percentage: number;
-  grade: string;
-}
 
 @Component({
   selector: 'app-test-score',
@@ -20,7 +12,7 @@ export interface ITest {
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<ITest> = [];
+  tests: Array<any> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
